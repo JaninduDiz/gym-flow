0e1e3f5c9c9f6496c8982639598b7982a22c7d6d
Janindu Dissanayake <janindu001@gmail.com>
2025-09-03 11:59:15 +0000

    fix this

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   projectId: 'gymflow-w4hud',
   appId: '1:190379023218:web:7eab4f64a22802cb0f5beb',
   storageBucket: 'gymflow-w4hud.firebasestorage.app',
-  apiKey: 'AIzaSyAKc_glfs3gELgQgR7aN_r9sULQbPJTA7k',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: 'gymflow-w4hud.firebaseapp.com',
   messagingSenderId: '190379023218',
 };
